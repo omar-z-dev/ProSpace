1fd5f335422a12fdb1941cf22c672fb116df029e
omar-z-dev <zaoudioo@gmail.com>
2026-09-09 13:03:36 +0200

    ff

--- public/js/form.js
+++ public/js/form.js
@@ -4,9 +4,12 @@ const nom = document.getElementById("nom");
 const email = document.getElementById("email");
 const entreprise = document.getElementById("entreprise");
 const typeDemande = document.getElementById("type-demande");
+const message = document.getElementById("message");
+const rgpd = document.getElementById("rgpd");
 
 const confirmation = document.getElementById("confirmation");
 
+/*************** Affichage de l'erreur ***************/
 function afficherErreur(champ, message) {
   const erreur = document.getElementById(
     `erreur-${champ.id === "type-demande" ? "type" : champ.id}`,
@@ -24,6 +27,7 @@ function supprimerErreur(champ) {
   erreur.textContent = "";
   champ.setAttribute("aria-invalid", "false");
 }
+/*************** NOM ***************/
 function validerNom() {
   if (nom.value.trim() === "") {
     afficherErreur(nom, "Le nom complet est obligatoire.");
@@ -33,7 +37,7 @@ function validerNom() {
   supprimerErreur(nom);
   return true;
 }
-
+/*************** EMAIL ***************/
 function validerEmail() {
   if (email.value.trim() === "") {
     afficherErreur(email, "L'adresse e-mail est obligatoire.");
@@ -48,7 +52,7 @@ function validerEmail() {
   supprimerErreur(email);
   return true;
 }
-
+/*************** ENTREPRISE ***************/
 function validerEntreprise() {
   if (entreprise.value.trim() === "") {
     afficherErreur(entreprise, "L'entreprise est obligatoire.");
@@ -58,7 +62,30 @@ function validerEntreprise() {
   supprimerErreur(entreprise);
   return true;
 }
+/*************** MESSAGE ***************/
+function validerMessage() {
+  if (message.value.trim() === "") {
+    afficherErreur(message, "Le message est obligatoire.");
+    return false;
+  }
 
+  supprimerErreur(message);
+  return true;
+}
+/*************** RGPD ***************/
+function validerRgpd() {
+  const erreur = document.getElementById("erreur-rgpd");
+
+  if (!rgpd.checked) {
+    erreur.textContent = "Vous devez accepter les conditions.";
+    rgpd.setAttribute("aria-invalid", "true");
+    return false;
+  }
+
+  erreur.textContent = "";
+  rgpd.setAttribute("aria-invalid", "false");
+  return true;
+}
 function validerTypeDemande() {
   if (typeDemande.value === "") {
     afficherErreur(typeDemande, "Veuillez sélectionner un type de demande.");
@@ -72,3 +99,32 @@ nom.addEventListener("input", validerNom);
 email.addEventListener("input", validerEmail);
 entreprise.addEventListener("input", validerEntreprise);
 typeDemande.addEventListener("change", validerTypeDemande);
+message.addEventListener("input", validerMessage);
+rgpd.addEventListener("change", validerRgpd);
+
+/*************** Envoi du formulaire au clic sur envoyer***************/
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nomValide = validerNom();
+  const emailValide = validerEmail();
+  const entrepriseValide = validerEntreprise();
+  const typeValide = validerTypeDemande();
+  const messageValide = validerMessage();
+  const rgpdValide = validerRgpd();
+
+  if (
+    !nomValide ||
+    !emailValide ||
+    !entrepriseValide ||
+    !typeValide ||
+    !messageValide ||
+    !rgpdValide
+  ) {
+    return;
+  }
+
+  confirmation.textContent = "Votre message a bien été envoyé.";
+  form.reset();
+});
